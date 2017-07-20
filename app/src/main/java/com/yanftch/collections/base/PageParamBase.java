5c58b4b17a44764851f7af94476ec19d5541b17b
yanftch <yanftch@163.com>
2017-07-20 08:29:32 +0800

    nothing

---
+++ app/src/main/java/com/yanftch/collections/base/PageParamBase.java
@@ -0,0 +1,12 @@
+package com.yanftch.collections.base;
+
+import java.io.Serializable;
+
+public class PageParamBase implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
